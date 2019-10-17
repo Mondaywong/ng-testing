7bc63b66390e689822c82a4158e468e64a8839f6
Mondaywong <mondayudowong@gmail.com>
2019-10-17 13:35:27 +0100

    2 files modified, 7 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,10 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { RedComponent } from './red/red.component';
-import { GreenComponent } from './green/green.component'
-import { YellowComponent } from './yellow/yellow.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule],
-  declarations: [ AppComponent, HelloComponent, RedComponent, GreenComponent, YellowComponent],
+  declarations: [ AppComponent, HelloComponentt],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- src/app/yellow/yellow.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-yellow',
-  templateUrl: './yellow.component.html',
-  styleUrls: ['./yellow.component.css']
-})
-export class YellowComponent  {
-}
