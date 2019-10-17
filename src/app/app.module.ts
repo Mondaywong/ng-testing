448d20aa14006ca10950282759d04b0f87be41b7
Mondaywong <mondayudowong@gmail.com>
2019-10-17 13:07:37 +0100

    3 files modified, 8 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,13 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RedComponent } from './red/red.component';
+import { GreenComponent } from './green/green.component'
+import { YellowComponent } from './yellow/yellow.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ BrowserModule, FormsModule],
+  declarations: [ AppComponent, HelloComponent, RedComponent, GreenComponent, YellowComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
